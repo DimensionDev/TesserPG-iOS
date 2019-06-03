00c4288c44bdc2a69392498b5b47df9776e657ab
CMK <cirno.mainasuk@gmail.com>
2019-06-03 16:46:43 +0800

    feat: complete message cell expand feature. resolve: https://github.com/DimensionDev/Tessercube-Meta/issues/32

--- TesserCube/Scene/Common/TCCardView.swift
+++ TesserCube/Scene/Common/TCCardView.swift
@@ -62,4 +62,45 @@ class TCCardView: UIView {
         layer.addSketchShadow(color: .black, alpha: 0.11, x: 0, y: 5, blur: 17, spread: 0, roundedRect: bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: cardCornerRadius, height: cardCornerRadius))
         glowLayer.frame = backgroundView.bounds
     }
+
+    override func action(for layer: CALayer, forKey event: String) -> CAAction? {
+        guard event == "shadowPath" else {
+            return super.action(for: layer, forKey: event)
+        }
+
+        guard let priorPath = layer.shadowPath else {
+            return super.action(for: layer, forKey: event)
+        }
+
+        guard let sizeAnimation = layer.animation(forKey: "bounds.size") as? CABasicAnimation else {
+            return super.action(for: layer, forKey: event)
+        }
+
+        // swiftlint:disable force_cast
+        let animation = sizeAnimation.copy() as! CABasicAnimation
+        // swiftlint:enable force_cast
+        animation.keyPath = "shadowPath"
+        let action = ShadowingViewAction()
+        action.priorPath = priorPath
+        action.pendingAnimation = animation
+        return action
+    }
+
+}
+
+// fix shadow render to target frame before animation finish issue
+// Ref: https://stackoverflow.com/a/47472010/3797903
+private class ShadowingViewAction: NSObject, CAAction {
+    var pendingAnimation: CABasicAnimation?
+    var priorPath: CGPath?
+
+    func run(forKey event: String, object anObject: Any, arguments dict: [AnyHashable : Any]?) {
+        guard let layer = anObject as? CALayer, let animation = self.pendingAnimation else {
+            return
+        }
+
+        animation.fromValue = self.priorPath
+        animation.toValue = layer.shadowPath
+        layer.add(animation, forKey: "shadowPath")
+    }
 }
