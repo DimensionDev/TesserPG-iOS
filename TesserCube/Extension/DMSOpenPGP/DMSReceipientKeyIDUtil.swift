1c3115a891cebd6c33895e15ddc1f059fc6bb302
jk234ert <ugling88@hotmail.com>
2019-06-14 14:00:26 +0800

    support decrypt messsage with hidden recipients key IDs

---
+++ TesserCube/Extension/DMSOpenPGP/DMSReceipientKeyIDUtil.swift
@@ -0,0 +1,34 @@
+//
+//  DMSReceipientKeyIDUtil.swift
+//  TesserCube
+//
+//  Created by jk234ert on 2019/6/12.
+//  Copyright © 2019 Sujitech. All rights reserved.
+//
+
+import Foundation
+
+extension Collection where Element == String
+{
+    func containsHiddenRecipientKeyID() -> Bool {
+        return contains { keyID -> Bool in
+            return keyID.isHiddenRecipientID
+        }
+    }
+    
+    func trimHiddenRecipientKeyID() -> [Element] {
+        return filter({ keyID -> Bool in
+            return !keyID.isHiddenRecipientID
+        })
+    }
+    
+    func getNumberOfHiddenRecipientKeyIDs() -> Int {
+        return count - trimHiddenRecipientKeyID().count
+    }
+}
+
+extension String {
+    var isHiddenRecipientID: Bool {
+        return replacingOccurrences(of: "0", with: "").isEmpty
+    }
+}
