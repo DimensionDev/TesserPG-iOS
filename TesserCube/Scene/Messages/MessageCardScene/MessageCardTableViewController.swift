68641d7d4cf4e5260968c5e903a4493b57b31b91
CMK <cirno.mainasuk@gmail.com>
2019-07-19 16:52:51 +0800

    feat: complete compose action extension and add MessageCardViewController to reduce redundant cod

---
+++ TesserCube/Scene/Messages/MessageCardScene/MessageCardTableViewController.swift
@@ -0,0 +1,170 @@
+//
+//  MessageCardTableViewController.swift
+//  TesserCube
+//
+//  Created by Cirno MainasuK on 2019-7-19.
+//  Copyright © 2019 Sujitech. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class MessageCardTableViewModel: NSObject {
+
+    private let disposeBag = DisposeBag()
+
+    // input
+    let messages = BehaviorRelay<[Message]>(value: [])
+    var messageExpandedDict: [IndexPath : Bool] = [:]
+    var messageMaxNumberOfLinesDict: [IndexPath : Int] = [:]
+
+}
+
+// MARK: - UITableViewDataSource
+extension MessageCardTableViewModel: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.value.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // swiftlint:disable force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MessageCardCell.self), for: indexPath) as! MessageCardCell
+        // swiftlint:enable force_cast
+
+        let message = messages.value[indexPath.row]
+        MessagesViewModel.configure(messageCardCell: cell, with: message)
+
+        if let isExpand = messageExpandedDict[indexPath],
+            let maxNumberOfLines = messageMaxNumberOfLinesDict[indexPath] {
+            cell.messageLabel.numberOfLines = isExpand ? 0 : 4
+            cell.extraBackgroundViewHeightConstraint.constant = maxNumberOfLines > 4 ? 44 : 0
+            let title = isExpand ? L10n.MessageCardCell.Button.Expand.collapse : L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
+            cell.expandButton.setTitle(title, for: .normal)
+        } else {
+            cell.messageLabel.layoutIfNeeded()
+            let maxNumberOfLines = cell.messageLabel.maxNumberOfLines
+            messageExpandedDict[indexPath] = false
+            messageMaxNumberOfLinesDict[indexPath] = maxNumberOfLines
+            cell.messageLabel.numberOfLines = 4
+            cell.extraBackgroundViewHeightConstraint.constant = maxNumberOfLines > 4 ? 44 : 0
+            let title = L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
+            cell.expandButton.setTitle(title, for: .normal)
+        }
+
+        cell.setNeedsLayout()
+        cell.layoutIfNeeded()
+
+        return cell
+    }
+
+}
+
+final class MessageCardTableViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+    let viewModel = MessageCardTableViewModel()
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.alwaysBounceVertical = true
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = 220
+        tableView.register(MessageCardCell.self, forCellReuseIdentifier: String(describing: MessageCardCell.self))
+        tableView.backgroundColor = .clear
+        tableView.keyboardDismissMode = .interactive
+        return tableView
+    }()
+
+}
+
+extension MessageCardTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Asset.sceneBackground.color
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+
+        tableView.dataSource = viewModel
+        tableView.delegate = self
+
+        viewModel.messages.asDriver()
+            .debug()
+            .drive(onNext: { [weak self] _ in
+                guard let `self` = self else { return }
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+extension MessageCardTableViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20 - MessageCardCell.cardVerticalMargin
+    }
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? MessageCardCell else {
+            return
+        }
+
+        cell.delegate = self
+
+        // Layout when cell display to make sure maxNumberOfLines calculated under right frame size
+        cell.setNeedsLayout()
+        cell.layoutIfNeeded()
+
+        let maxNumberOfLines = cell.messageLabel.maxNumberOfLines
+        viewModel.messageMaxNumberOfLinesDict[indexPath] = maxNumberOfLines
+        let isExpand = viewModel.messageExpandedDict[indexPath] ?? false
+        let title = isExpand ? L10n.MessageCardCell.Button.Expand.collapse : L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
+        cell.expandButton.setTitle(title, for: .normal)
+    }
+
+}
+
+// MARK: - MessageCardCellDelegate
+extension MessageCardTableViewController: MessageCardCellDelegate {
+
+    func messageCardCell(_ cell: MessageCardCell, expandButtonPressed: UIButton) {
+        guard let indexPath = tableView.indexPath(for: cell),
+            let isExpand = viewModel.messageExpandedDict[indexPath],
+            let maxNumberOfLines = viewModel.messageMaxNumberOfLinesDict[indexPath] else {
+                return
+        }
+
+        cell.messageLabel.numberOfLines = isExpand ? 4 : 0
+        viewModel.messageExpandedDict[indexPath] = !isExpand
+        let title = !isExpand ? L10n.MessageCardCell.Button.Expand.collapse : L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
+        cell.expandButton.setTitle(title, for: .normal)
+
+        tableView.beginUpdates()
+        tableView.endUpdates()
+
+        tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
+
+}
--- TesserCubeInterpretAction/InterpretActionViewModel.swift
+++ TesserCubeInterpretAction/InterpretActionViewModel.swift
@@ -11,52 +11,37 @@ import RxSwift
 import RxCocoa
 import DMSOpenPGP
 
-protocol InterpretActionViewModelDelegate: class {
-    func writeReply(to recipients: [KeyBridge], from sender: KeyBridge?)
-}
-
 final class InterpretActionViewModel: NSObject {
 
     private let disposeBag = DisposeBag()
-    weak var delegate: InterpretActionViewModelDelegate?
 
     // Input
     var inputTexts: [String] = []
-    var messageExpandedDict: [IndexPath : Bool] = [:]
-    var messageMaxNumberOfLinesDict: [IndexPath : Int] = [:]
     var copyAction = PublishRelay<UIButton>()
     var replyAction = PublishRelay<UIButton>()
+    var composedMessage = BehaviorRelay<Message?>(value: nil)
 
     // Output
     let title = BehaviorRelay<String>(value: L10n.InterpretActionViewController.Title.messageInterpreting)
     let armoredMessage = BehaviorRelay<String?>(value: nil)
-    let message = BehaviorRelay<Message?>(value: nil)
-    let availableActions = BehaviorRelay<[Action]>(value: [])
+    let interpretedMessage = BehaviorRelay<Message?>(value: nil)
+    let messages: Driver<[Message]>
 
     override init() {
+        messages = Driver.combineLatest(interpretedMessage.asDriver(), composedMessage.asDriver()) { interpretedMessage, composedMessage in
+            return [interpretedMessage, composedMessage].compactMap { $0 }
+        }
+
         super.init()
 
         armoredMessage.asDriver()
             .flatMapLatest { message in
                 return Driver.just(message.flatMap { try? ProfileService.default.decryptMessage($0) })
             }
-            .drive(message)
+            .drive(interpretedMessage)
             .disposed(by: disposeBag)
 
-        message.asDriver()
-            .drive(onNext: { [weak self] message in
-                guard let message = message, let `self` = self else { return }
-
-                // set available actions
-                if ProfileService.default.containsKey(longIdentifier: message.senderKeyId) {
-                    self.availableActions.accept([.copy, .reply])
-                } else {
-                    self.availableActions.accept([.copy])
-                }
-            })
-            .disposed(by: disposeBag)
-
-        message.asDriver()
+        interpretedMessage.asDriver()
             .skip(1)
             .drive(onNext: { message in
                 let title = message != nil ? L10n.InterpretActionViewController.Title.messageInterpreted : L10n.InterpretActionViewController.Title.brokenMessage
@@ -64,53 +49,8 @@ final class InterpretActionViewModel: NSObject {
             })
             .disposed(by: disposeBag)
 
-        // handler actions
-        copyAction
-            .observeOn(MainScheduler.asyncInstance)
-            .do(onNext: { sender in
-                sender.setTitle(L10n.InterpretActionViewController.Action.Button.copied, for: .normal)
-                UIPasteboard.general.string = self.message.value?.rawMessage
-            })
-            .debounce(1, scheduler: MainScheduler.asyncInstance)
-            .delay(1, scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { sender in
-                sender.setTitle(L10n.InterpretActionViewController.Action.Button.copyContent, for: .normal)
-            })
-            .disposed(by: disposeBag)
 
-        replyAction
-            .observeOn(MainScheduler.asyncInstance)
-            .debounce(1, scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { [weak self] sender in
-                guard let message = self?.message.value else {
-                    return
-                }
-
-                let sender = message
-                    .getRecipients()
-                    .first(where: { recipient in
-                        // select first recipient's key, which user has it's secret key, as sender key to reply
-                        guard let key = recipient.getKey(), key.hasPublicKey, key.hasSecretKey else { return false }
-                        return true
-                    })
-                    .flatMap { $0.getKey() }
-                    .map { KeyBridge(contact: nil, key: $0, userID: $0.userID, longIdentifier: $0.longIdentifier) }
-
-                let recipient = KeyBridge(contact: nil, key: nil, userID: message.senderKeyUserId, longIdentifier: message.senderKeyId)
-
-                self?.delegate?.writeReply(to: [recipient], from: sender)
-            })
-            .disposed(by: disposeBag)
     }   // end init()
-
-}
-
-extension InterpretActionViewModel {
-
-    enum Action {
-        case copy
-        case reply
-    }
 
 }
 
@@ -127,47 +67,3 @@ extension InterpretActionViewModel {
 
 }
 
-// MARK: - UITableViewDataSource
-extension InterpretActionViewModel: UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return message.value != nil ? 1 : 0
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MessageCardCell.self), for: indexPath) as! MessageCardCell
-
-        guard let message = message.value else {
-            return cell
-        }
-
-        MessagesViewModel.configure(messageCardCell: cell, with: message)
-
-        if let isExpand = messageExpandedDict[indexPath],
-            let maxNumberOfLines = messageMaxNumberOfLinesDict[indexPath] {
-            cell.messageLabel.numberOfLines = isExpand ? 0 : 4
-            cell.extraBackgroundViewHeightConstraint.constant = maxNumberOfLines > 4 ? 44 : 0
-            let title = isExpand ? L10n.MessageCardCell.Button.Expand.collapse : L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
-            cell.expandButton.setTitle(title, for: .normal)
-        } else {
-            cell.messageLabel.layoutIfNeeded()
-            let maxNumberOfLines = cell.messageLabel.maxNumberOfLines
-            messageExpandedDict[indexPath] = false
-            messageMaxNumberOfLinesDict[indexPath] = maxNumberOfLines
-            cell.messageLabel.numberOfLines = 4
-            cell.extraBackgroundViewHeightConstraint.constant = maxNumberOfLines > 4 ? 44 : 0
-            let title = L10n.MessageCardCell.Button.Expand.expand(maxNumberOfLines)
-            cell.expandButton.setTitle(title, for: .normal)
-        }
-
-        cell.setNeedsLayout()
-        cell.layoutIfNeeded()
-
-        return cell
-    }
-
-}
