d8fa9eb5a2babd6c21e0ad45c62d6ff17071c740
CMK <cirno.mainasuk@gmail.com>
2019-08-02 15:07:43 +0800

    feat: adopt dark mode in Message scenes

--- TesserCube/Scene/Messages/ComposeMessageScene/View/Cell/ContactPickerTagCollectionViewCell.swift
+++ TesserCube/Scene/Messages/ComposeMessageScene/View/Cell/ContactPickerTagCollectionViewCell.swift
@@ -37,7 +37,12 @@ final class ContactPickerTagCollectionViewCell: UICollectionViewCell {
 
     let corneredBackgroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = Asset.tagBackgroundGrey.color
+        if #available(iOS 13.0, *) {
+            view.backgroundColor = .secondarySystemBackground
+        } else {
+            // Fallback on earlier versions
+            view.backgroundColor = Asset.tagBackgroundGrey.color
+        }
         return view
     }()
 
@@ -47,13 +52,19 @@ final class ContactPickerTagCollectionViewCell: UICollectionViewCell {
         label.font = FontFamily.SFProDisplay.regular.font(size: 16)
         label.adjustsFontSizeToFitWidth = true
         label.minimumScaleFactor = 0.8
+        if #available(iOS 13.0, *) {
+            label.textColor = .label
+        } else {
+            // Fallback on earlier versions
+            label.textColor = .black
+        }
         return label
     }()
 
     let shortIDLabel: UILabel = {
         let label = UILabel()
         label.font = FontFamily.SourceCodeProMedium.regular.font(size: 11)
-        label.textColor = Asset.tagIdGreen.color
+        label.textColor = .systemGreen
         label.numberOfLines = 2
         return label
     }()
@@ -74,11 +85,16 @@ final class ContactPickerTagCollectionViewCell: UICollectionViewCell {
     var isInvalid = false {
         didSet {
             if isInvalid {
-                corneredBackgroundView.backgroundColor = Asset.tagBackgroundPink.color
-                shortIDLabel.textColor = Asset.tagIdRed.color
+                corneredBackgroundView.backgroundColor = .systemPink
+                shortIDLabel.textColor = .systemRed
             } else {
-                corneredBackgroundView.backgroundColor = Asset.tagBackgroundGrey.color
-                shortIDLabel.textColor = Asset.tagIdGreen.color
+                if #available(iOS 13.0, *) {
+                    corneredBackgroundView.backgroundColor = .secondarySystemBackground
+                } else {
+                    // Fallback on earlier versions
+                    corneredBackgroundView.backgroundColor = Asset.tagBackgroundGrey.color
+                }
+                shortIDLabel.textColor = .systemGreen
             }
         }
     }
@@ -159,16 +175,23 @@ extension ContactPickerTagCollectionViewCell {
             if isSelected {
                 if !isFirstResponder { becomeFirstResponder() }
                 UIView.animate(withDuration: 0.33) {
-                    self.corneredBackgroundView.backgroundColor = Asset.sketchBlue.color
+                    self.corneredBackgroundView.backgroundColor = .systemBlue
                     self.nameLabel.textColor = .white
                     self.shortIDLabel.textColor = .white
                 }
             } else {
                 if isFirstResponder { resignFirstResponder() }
                 UIView.animate(withDuration: 0.33) {
-                    self.corneredBackgroundView.backgroundColor = Asset.tagBackgroundGrey.color
-                    self.nameLabel.textColor = .black
-                    self.shortIDLabel.textColor = Asset.tagIdGreen.color
+                    if #available(iOS 13.0, *) {
+                        self.corneredBackgroundView.backgroundColor = .secondarySystemBackground
+                        self.nameLabel.textColor = .label
+                    } else {
+                        // Fallback on earlier versions
+                        self.corneredBackgroundView.backgroundColor = Asset.tagBackgroundGrey.color
+                        self.nameLabel.textColor = .black
+                    }
+
+                    self.shortIDLabel.textColor = .systemGreen
                     let isInvalid = self.isInvalid
                     self.isInvalid = isInvalid
                 }
