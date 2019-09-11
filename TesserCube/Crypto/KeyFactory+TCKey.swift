949ca1819811151ad6f508b50ff009527b5db3d1
CMK <cirno.mainasuk@gmail.com>
2019-09-11 16:28:06 +0800

    fix: not prompt "invalid password" when import private key with wrong passphrase issue

--- TesserCube/Crypto/KeyFactory+TCKey.swift
+++ TesserCube/Crypto/KeyFactory+TCKey.swift
@@ -14,9 +14,7 @@ extension KeyFactory {
 
     static func key(from armoredKey: String, passphrase: String?) throws -> TCKey {
         do {
-            guard let keyRing = try? DMSPGPKeyRing(armoredKey: armoredKey, password: passphrase) else {
-                throw TCError.pgpKeyError(reason: .invalidKeyFormat)
-            }
+            let keyRing = try DMSPGPKeyRing(armoredKey: armoredKey, password: passphrase)
             let key = TCKey(keyRing: keyRing, from: nil)
 
             // Check passphrase if there is secret key within
@@ -30,6 +28,8 @@ extension KeyFactory {
 
             return key
 
+        } catch DMSPGPError.invalidSecrectKeyPassword {
+            throw TCError.pgpKeyError(reason: .invalidPassword)
         } catch let error as DMSPGPError {
             consolePrint(error.localizedDescription)
             throw TCError.pgpKeyError(reason: .invalidKeyFormat)
