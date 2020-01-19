e8541541fc2942b5f0af35482f3d05a99aa944e6
CMK <cirno.mainasuk@gmail.com>
2020-01-19 16:09:11 +0800

    fix: add token image not load when first time appeared due to zero frame issue

--- TesserCube/Scene/Wallets/AddTokenScene/Cell/TokenTableViewCell.swift
+++ TesserCube/Scene/Wallets/AddTokenScene/Cell/TokenTableViewCell.swift
@@ -16,7 +16,7 @@ final class TokenTableViewCell: UITableViewCell {
     var disposeBag = DisposeBag()
     
     let logoImageView: UIImageView = {
-        let imageView = UIImageView()
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         imageView.layer.cornerRadius = 4
         imageView.layer.masksToBounds = true
         return imageView
